9e6bf8068042791be8aa709ad36b5eae07feb4d9
Luis <luisrobertolrm@gmail.com>
2024-12-16 16:24:54 -0300

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
